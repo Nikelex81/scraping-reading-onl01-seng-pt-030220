f60c1067ec84630f6d4ecf3684c7b6527db2021e
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-12 18:46:38 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,3 +7,7 @@ doc = Nokogiri::HTML(open("https://flatironschool.com/"))
 
 doc.css(".headline-26OIBN").text
 puts doc.css(".headline-26OIBN")
+courses = doc.css(".title-3Kf9MY")
+courses.each do |course|
+  puts course.text.strip 
+end
