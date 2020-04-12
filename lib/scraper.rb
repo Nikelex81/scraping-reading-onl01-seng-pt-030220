7f497c16bd141f5a2d204e15a04190027eb19115
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-12 18:35:30 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 # html = open("https://flatironschool.com/")
 
-doc = Nokogiri::HTML(html)
+doc = Nokogiri::HTML(open())
 
 doc.css(".headline-26OIBN").text
 puts doc.css(".headline-26OIBN")
