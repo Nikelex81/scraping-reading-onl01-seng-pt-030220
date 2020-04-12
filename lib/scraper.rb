3759c9d7955f10e03d44fcfefed78d6459fc7889
Nikelson Exantus <nikelsone1981@hotmail.com>
2020-04-12 19:04:48 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,16 @@
 require 'nokogiri'
 require 'open-uri'
 
-html = open("https://flatironschool.com/")
+# html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(open("https://flatironschool.com/"))
+
+doc.css(".headline-26OIBN").text
+puts doc.css(".headline-26OIBN")
+courses = doc.css(".title-oE5vT4")
+courses.each do |course|
+  puts course.text.strip 
+end
+
+p doc.css(".title-oE5vT4")[0].name 
+p doc.css(".title-oE5vT4")[0].attributes
